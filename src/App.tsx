f25aa0e122fe41dd24eeedb94b6d03266ad5b4d0
Marina Efimova <mef1983@yandex.ru>
2022-06-24 11:37:19 +0300

    event and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NameComponent from './NameComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <div>
+    <header className="page-header">
+      <img className="page-header__logo" src="./logo.svg" alt="Купи самовар" width="152" />
+      <nav className="page-header__menu">
+        <ul className="mainmenu__list">
+          <li className="mainmenu__item">
+            <a href="#!">О НАС</a>
+          </li>
+          <li className="mainmenu__item">
+            <a href="#!">САМОВАРЫ</a>
+          </li>
+          <li className="mainmenu__item">
+            <a href="#!">ДОСТАВКА</a>
+          </li>
+          <li className="mainmenu__item">
+            <a href="#!">КОНТАКТЫ</a>
+          </li>
+        </ul>
+      </nav>
+      <NameComponent firstName="Марина" lastName="Ефимова" />
+    </header>
+  </div>
+);
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// const element = <p className="text">Hello React</p>;
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
